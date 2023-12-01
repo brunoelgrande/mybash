e7bc7e88d5cffd8ab534bc12df75fad6e664b791
Bruno Gauthier <90357412+brunoelgrande@users.noreply.github.com>
2023-12-01 17:01:00 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -75,7 +75,7 @@ checkEnv() {
 
 installDepend() {
     ## Check for dependencies.
-    DEPENDENCIES='autojump bash bash-completion tar neovim bat'
+    DEPENDENCIES='autojump bash bash-completion tar trash-cli bat'
     echo -e "${YELLOW}Installing dependencies...${RC}"
     if [[ $PACKAGER == "pacman" ]]; then
         if ! command_exists yay; then
