181dd0bad06215980dcf2600eb7cc57de5c85283
Bruno Gauthier <90357412+brunoelgrande@users.noreply.github.com>
2024-01-09 13:04:32 -0500

    Create fonts.sh
    Script pour installation automatique des fonts principaux désirés.

---
+++ fonts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo apt install unzip -y
+
+mkdir -p ~/.local/share/fonts
+
+cd /tmp
+fonts=( 
+"CascadiaCode"
+"FiraCode" 
+"Go-Mono" 
+"Hack"  
+"Iosevka" 
+"JetBrainsMono" 
+"Mononoki" 
+"Meslo"
+"RobotoMono" 
+"SourceCodePro" 
+"UbuntuMono"
+)
+
+for font in ${fonts[@]}
+do
+    # wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/$font.zip
+    wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/$font.zip
+	unzip $font.zip -d $HOME/.local/share/fonts/$font/
+    rm $font.zip
+done
+fc-cache
